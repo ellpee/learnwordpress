5ffd40069c2ec0ebd34037beaaea08729c4eaf0d
ellpee <lporter630@gmail.com>
2016-12-02 13:01:52 -0500

    Created a better tag and theme description. Added Skeleton HTML to the index php page.

--- wordpress2016/wp-content/themes/creative-entrepreneur-blog-theme/index.php
+++ wordpress2016/wp-content/themes/creative-entrepreneur-blog-theme/index.php
@@ -1,3 +1,25 @@
-<?php
-echo "Hell World";
- ?>
+<html>
+<head>
+  <title>
+    Creative Entrepreneur Blog Theme
+  </title>
+  <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<header>
+  Header Content, logo, menu, and etc.
+</header>
+<aside>
+  Left Sidebar
+</aside>
+<section>
+  Body Content
+</section>
+<aside>
+  Right Sidebar
+</aside>
+<footer>
+  Footer Content, Copyright, Designed By 
+</footer>
+</body>
+</html>
